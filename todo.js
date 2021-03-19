10ea5babb75bc924211c3eed1683e2fdee645a12
lee-tae-eon <mktears0609@gmail.com>
2021-03-19 21:27:01 +0900

    background commit

--- todo.js
+++ todo.js
@@ -1,25 +1,67 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-    toDoInput = form.querySelector("input"),
+    toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    // filter() - 각가의 item에 필터링
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    console.log(cleanToDos);
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+ 
 function paintToDo(text){
-    
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerHTML = "×";
+    delBtn.addEventListener("click", deleteToDo);
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
 }
+
+
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
-    };
+    const loadToDos = localStorage.getItem(TODOS_LS);
+    if(loadToDos !== null){
+        const parsedToDos = JSON.parse(loadToDos);
+        // forEach - array에 담겨있는 것들 각각에 함수를 한번씩 실행시켜줌
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
+    }
 }
-
 
 function init() {
     loadToDos();
