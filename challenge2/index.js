4393192bb3fc2a4256b6926aa07f8ca34afea309
lee-tae-eon <mktears0609@gmail.com>
2021-04-22 17:14:36 +0900

    upadate challenge2

--- challenge2/index.js
+++ challenge2/index.js
@@ -1,14 +1,19 @@
 const selectBody = document.querySelector("body");
 
+const h2 = document.createElement("h2");
+selectBody.appendChild(h2);
+h2.innerHTML = "HELLO!";
+h2.style.color = "white";
+
 const BASE_COLOR = "#3498DB";
-const PUPPLE_COLOR = "#74418B";
+const PURPLE_COLOR = "#74418B";
 const YELLOW_COLOR = "yellow";
 const changeColor = () => {
-  const currentWidth = window.innerWidth;
+  const currentWidth = window.outerWidth;
   if (currentWidth > 1200) {
     selectBody.style.backgroundColor = YELLOW_COLOR;
-  } else if (currentWidth >= 800 && currentWidth < 1200) {
-    selectBody.style.backgroundColor = PUPPLE_COLOR;
+  } else if (currentWidth >= 600 && currentWidth < 1200) {
+    selectBody.style.backgroundColor = PURPLE_COLOR;
   } else {
     selectBody.style.backgroundColor = BASE_COLOR;
   }
