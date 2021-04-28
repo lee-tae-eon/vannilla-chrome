e1b23124adab4e9f747a174b9b0fb53837793cf9
lee-tae-eon <mktears0609@gmail.com>
2021-04-28 20:39:43 +0900

    challenge6 update

---
+++ challenge6/index.js
@@ -0,0 +1,35 @@
+const h2 = document.querySelector("h2");
+const form = document.querySelector(".js-form");
+const rangeInput = document.querySelector(".range-input");
+const numberInput = document.querySelector(".number-input");
+const selectText = document.querySelector(".select-text");
+const resultText = document.querySelector(".result-text");
+const button = document.querySelector("button");
+
+numberInput.style.width = "33px";
+
+const handleRange = function () {
+  h2.innerText = `Generate a number between 0 and ${this.value}`;
+};
+const handleButton = () => {
+  const selectNumber = rangeInput.value * 1;
+  const choseNumber = numberInput.value * 1;
+  const randNumber = Math.floor(Math.random() * (selectNumber + 1));
+  selectText.innerHTML = `You chose : ${choseNumber} , Machine chose :${randNumber}`;
+  const winText = "You Win";
+  const lostText = "You Lost";
+  if (choseNumber === randNumber) {
+    resultText.innerHTML = winText;
+    if (resultText.outerText == "You Win") {
+      resultText.style.color = "red";
+    } else {
+      resultText.style.color = "black";
+    }
+  } else {
+    resultText.innerHTML = lostText;
+  }
+};
+
+form.addEventListener("submit", (e) => e.preventDefault());
+rangeInput.addEventListener("input", handleRange);
+button.addEventListener("click", handleButton);
